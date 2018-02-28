e69cb7566296b05f28c825028e0d9cf7e80a6978
Chris Mika <chris.mika@nymag.com>
2018-02-28 14:10:29 -0500

    handle other envs

--- scripts/aws/ApplicationStart.sh
+++ scripts/aws/ApplicationStart.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 # start application
-systemctl start ${APPLICATION_NAME}-3001
-systemctl start ${APPLICATION_NAME}-3002
+ENV=$(echo ${DEPLOYMENT_GROUP_NAME} | awk -F\- '{print $NF}')
+if [[ ${ENV} == 'prd' ]]; then
+  systemctl start ${APPLICATION_NAME}-3001
+  systemctl start ${APPLICATION_NAME}-3002
+else
+  systemctl start ${APPLICATION_NAME}
+fi
